dfa78d7d6d8d849da52bc3d6dd63415839a82726
aldotestino <aldotestino4@gmail.com>
2024-04-23 19:32:46 +0200

    extract spinner component + env variable

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Patient, PatientPrediction } from './types';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_BACKEND_URL as string
 });
 
 export async function predict(values: Patient) {
