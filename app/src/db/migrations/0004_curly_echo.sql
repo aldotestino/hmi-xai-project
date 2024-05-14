057c42217de232f551c378fcf3c2a06cf41b15de
aldotestino <aldotestino4@gmail.com>
2024-05-14 21:54:10 +0200

    update migration

---
+++ app/src/db/migrations/0004_curly_echo.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "patient_prediction" ALTER COLUMN "bmi" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "patient_prediction" ALTER COLUMN "diabetes_pedigree_function" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "patient_prediction" ALTER COLUMN "prediction" SET DATA TYPE double precision;--> statement-breakpoint
+ALTER TABLE "patient_prediction" ALTER COLUMN "shap_base_value" SET DATA TYPE double precision;
