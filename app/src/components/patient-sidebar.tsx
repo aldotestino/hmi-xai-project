5436371e75c062042ce7bf5dfc9d2f00a2aa04b9
aldotestino <aldotestino4@gmail.com>
2024-05-16 17:09:37 +0200

    fix sidebar

--- app/src/components/patient-sidebar.tsx
+++ app/src/components/patient-sidebar.tsx
@@ -16,19 +16,17 @@ function PatientSidebar({
 }) {
 
   return (
-    <div className="h-full overflow-y-hidden flex items-center">
-      <div className="bg-white w-80 h-full border-r grid grid-rows-[auto,1fr]">
-        <div>
-          <div className='flex items-center gap-2 p-4'>
-            <Link href="/" className={cn(buttonVariants({ variant: 'ghost' }), 'w-8 h-8 p-0')}> 
-              <ArrowLeft className='w-4 h-4' />
-            </Link>
-            <h1 className="text-xl font-semibold">{patient.firstName} {patient.lastName}</h1>
-          </div>
-          <Separator />
+    <div className='bg-white w-80 h-full border-r grid grid-rows-[auto,1fr] overflow-y-hidden'>
+      <div>
+        <div className='flex items-center gap-2 p-4'>
+          <Link href="/" className={cn(buttonVariants({ variant: 'ghost' }), 'w-8 h-8 p-0')}> 
+            <ArrowLeft className='w-4 h-4' />
+          </Link>
+          <h1 className="text-xl font-semibold">{patient.firstName} {patient.lastName}</h1>
         </div>
-        <PatientFeaturesForm patient={patient} defaultValues={lastFeatueres} />
+        <Separator />
       </div>
+      <PatientFeaturesForm patient={patient} defaultValues={lastFeatueres} />
     </div>
   );
 }
